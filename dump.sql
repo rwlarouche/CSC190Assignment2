5a2838e3f910648f6e5ff4c1eef8d3a8b1df42a6
Ryan LaRouche <rwlarouche@yahoo.com>
2018-03-08 20:16:34 -0500

    Uploaded MASTER to RELEASE1

---
+++ dump.sql
@@ -0,0 +1,51 @@
+-- MySQL dump 10.13  Distrib 5.6.35, for Linux (x86_64)
+--
+-- Host: localhost    Database: db_reports
+-- ------------------------------------------------------
+-- Server version	5.6.35
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tbl_reports`
+--
+
+DROP TABLE IF EXISTS `tbl_reports`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_reports` (
+  `nname` varchar(255) DEFAULT NULL,
+  `msg` varchar(1024) DEFAULT NULL,
+  `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_reports`
+--
+
+LOCK TABLES `tbl_reports` WRITE;
+/*!40000 ALTER TABLE `tbl_reports` DISABLE KEYS */;
+INSERT INTO `tbl_reports` VALUES ('ryan','test message','2018-03-09 00:01:36'),('','Adams!','2018-03-09 00:08:30'),('','CV Star','2018-03-09 00:08:40'),('','CV Star','2018-03-09 00:37:46');
+/*!40000 ALTER TABLE `tbl_reports` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-03-08 19:51:45
