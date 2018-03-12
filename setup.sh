8d3040396dd24ab88fdfb2dd4a95d19484a5dcf7
Craig <cmetzdorff1@pride.hofstra.edu>
2018-03-11 22:09:17 -0400

    T10 Altered setup.sh to allow it to be run from the context of any directory instead of having to cd to its containing directory first.

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
-cp -R www/* /var/www/html
-mysql -u root -pgoodyear123!@# < dbschema.sql
+#!/bin/bash
+#Copies php files over to www and inject the db schema into mysql.  Should change password in use before deploying to production.
+cp -R `dirname $0`/www/* /var/www/html
+mysql -u root -pgoodyear123!@# < `dirname $0`/dbschema.sql
